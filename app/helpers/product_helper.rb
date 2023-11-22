de8bd8a9db9b122b8df6fa0c15781d13e3ffee9b
Garrick Henne <garrickhenne@outlook.com>
2023-11-22 15:24:35 -0800

    Render default photo if no photo exists for products

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -2,4 +2,8 @@ module ProductHelper
   def sold_out? (product)
     product.quantity <= 0
   end
+
+  def img?(url)
+    url || 'https://i.pinimg.com/originals/2a/b1/3a/2ab13a4cebbf0cb6dd2cd5aa7e59abee.png'
+  end
 end
