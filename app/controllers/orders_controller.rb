d27547cf335d326a2794d504e840a0914775ff93
Garrick Henne <garrickhenne@outlook.com>
2023-11-22 15:25:03 -0800

    Fix bug passing incorrect arguments into create new line_items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,12 +44,12 @@ class OrdersController < ApplicationController
     enhanced_cart.each do |entry|
       product = entry[:product]
       quantity = entry[:quantity]
-      order.line_items.new(
-        product,
-        quantity,
+      order.line_items.new({
+        product: product,
+        quantity: quantity,
         item_price: product.price,
         total_price: product.price * quantity
-      )
+      })
     end
     order.save!
     order
